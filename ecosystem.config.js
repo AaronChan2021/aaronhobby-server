34cffe2dd217afff4221db443f9f5b405217859a
Aaron <aaronchan.2021@gmail.com>
2025-04-18 13:26:34 +0800

    delete output

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,22 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: 'aaronhobby-server',
-      script: 'dist/server.js',
-      env: {
-        NODE_ENV: 'development'
-      },
+      script: './dist/server.js',
       env_production: {
-        NODE_ENV: 'production'
-      }
+        NODE_ENV: 'production',
+        PORT: process.env.PORT,
+        // 确保这些环境变量与 .env 文件中的一致
+        DATABASE_URL: process.env.DATABASE_URL,
+        JWT_SECRET: process.env.JWT_SECRET
+      },
+      error_file: 'logs/err.log', // 错误日志路径
+      out_file: 'logs/out.log', // 输出日志路径
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      instances: 1,
+      exec_mode: 'fork',
+      max_memory_restart: '500M'
     }
   ]
 }
--- readme.md
+++ readme.md
@@ -9,3 +9,8 @@ npx prisma generate
 npm run build
 // 服务器需安装 pm2
 pm2 start ecosystem.config.js --env production
+// 查看日志
+pm2 logs aaronnhobby-server
+// 停止，删除所有pm2进程
+pm2 stop all
+pm2 delete all
